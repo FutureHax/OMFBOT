7e1d64fd282ff1e8fd3cb8948b432297572ed420
r2DoesInc <r2doesinc@poiteeserver>
2011-06-24 02:23:45 -0400

    Update device manifests as new builds become available. Add more user info to the scripts Update .gitignore

--- Nightly.sh
+++ Nightly.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+#Announce the beginning of nightlies.
 ttytter -status="Nightlies for $DATE have started, stay tuned"
 
+#Pull config from file.
 . OMFBOT/OMFBOT_config
 
-echo "$DATE"
-
+#Pull in all new changes, and reset back to HEAD to be sure no testing commits are included.
 pushd ~/Nightly
+repo forall -c git pull origin master
+repo forall -c git pull korg gingerbread-release
+repo forall -c git pull cm gingerbread
 repo forall -c git reset HEAD --hard
+
+#Specify nightly rather than full build.
 . vendor/omfgb/build/nightly.sh
 popd
 
+#Start builds. These can be reordered.
 ./OMFBOT/inc.sh
 
 ./OMFBOT/sholes.sh
--- upload.sh
+++ upload.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Pull in info from config file.
 . ~/OMFBOT/OMFBOT_config
 
+#Upload file to FTP
 ftp -n -v $HOST << EOT
 ascii
 user $USER $PASSWD
@@ -12,8 +14,22 @@ put $ZIP
 bye
 EOT
 
-pushd ~/Nightly
-make clobber
+#Announce new Nightly build.
+ttytter -status="New $DEVICE nightly available http://r2doesinc.bitsurge.net/nightlies/$DEVICE/$ZIP"
+
+#Update device nightky manifests.
+pushd ~/OMFBOT/
+rm -rf $MANIFESTDIR
+git clone $MANIFESTREPO
+pushd $MANIFESTDIR
+git checkout -b master origin/master
+git checkout master
+sed -i "1 a\\$MANIFESTENTRY" $DEVICE.js
+git add $DEVICE.js
+git commit -m "New $DATE nightly for $DEVICE. Via @OMFBOT"
+git push
+popd
 popd
 
-ttytter -status="New $DEVICE nightly available http://r2doesinc.bitsurge.net/nightlies/$DEVICE/$ZIP"
+#Announce new build avaialibility in God Mode.
+ttytter -status="Grab the new $DEVICE nightly from God Mode!"
