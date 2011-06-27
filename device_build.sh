b44bbc847d394675ba9c5d9345755b8268a87ce7
jdkoreclipse <jdkoreclipse@gmail.com>
2011-06-27 19:18:05 -0400

    clean it up a bit
    Set it to make the /build directory in the kernel source, remove any compiled files in the /build dir, and fix the cp issue with the evo files'

--- device_build.sh
+++ device_build.sh
@@ -5,6 +5,9 @@
 
 #cd to kernel soure path, assuming your kernel soure dir is named src.
 cd ~/src
+
+#make  /build dir for all the compilef files
+mkdir build
  
 #self explanitory....this does not chnge.
 make clean
@@ -15,6 +18,15 @@ make device_defconfig
 #your exports (this line doesnt change unless you are using a custom toolchain).
 export CCOMPILER=${HOME}/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
 
+#delete any files left in /build dir of kernel src
+rm ~/src/build/zImage
+rm  ~/src/build/system/lib/modules/bcm4329.ko
+
+#evo specific files
+#rm ~/src/build/system/lib/modules/sequans_sdio.ko
+#rm ~/src/build/system/lib/modules/wimaxdbg.ko
+#rm ~/src/build/system/lib/modules/wimaxuart.ko
+
 #make the build.
 make ARCH=arm CROSS_COMPILE=$CCOMPILER -j`grep 'processor' /proc/cpuinfo | wc -l`
 
@@ -23,11 +35,11 @@ cp ~/src/arch/arm/boot/zImage ~/src/build/zImage
 cp ~/src/drivers/net/wireless/bcm4329/bcm4329.ko  ~/src/build/system/lib/modules/bcm4329.ko
 
 #these files are evo-specific
-#cp ~/src/drivers/net/wimax/SQN/sequans_sdio.ko ~/src/build/system/lib/modules/
-#cp ~/src/drivers/net/wimax/wimaxdbg/wimaxdbg.ko ~/src/build/system/lib/modules/
-#cp ~/src/drivers/net/wimax/wimaxuart/wimaxuart.ko ~/src/build/system/lib/modules/
+#cp ~/src/drivers/net/wimax/SQN/sequans_sdio.ko ~/src/build/system/lib/modules/sequans_sdio.ko
+#cp ~/src/drivers/net/wimax/wimaxdbg/wimaxdbg.ko ~/src/build/system/lib/modules/wimaxdbg.ko
+#cp ~/src/drivers/net/wimax/wimaxuart/wimaxuart.ko ~/src/build/system/lib/modules/wimaxuart.ko
 
-#go back to your build dir
+#go to your build dir
 cd build
 
 #zip the kernel up 
